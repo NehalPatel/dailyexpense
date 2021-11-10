b889aa007a5374c6dd2496ed45c41f1adb02cece
Nehal Patel <patelnehal4u@gmail.com>
2021-11-10 23:28:46 +0530

    migration tables created

---
+++ database/migrations/2021_11_10_172139_create_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('icon',100)->nullable()->default(null);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
---
+++ database/migrations/2021_11_10_172255_create_expenses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->double('amount', 8, 2)->default(0.0);
+            $table->foreignId('category_id')->constrained();
+            $table->string('payment_method')->default('cash');
+            $table->foreignId('bank_id')->nullable()->constrained();
+            $table->unsignedTinyInteger('payment_gateway')->default(0);
+            $table->string('cheque_no')->nullable();
+            $table->string('place', 255)->nullable()->default(null);
+            $table->string('description', 255)->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
